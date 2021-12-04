22962e71305ae7fa097ecb41d0669b3d48533992
bogdanKukliuk <33459108+bogdanKukliuk@users.noreply.github.com>
2021-12-04 16:30:44 +0200

    first

---
+++ sql/init.sql
@@ -0,0 +1,7 @@
+Use test;
+CREATE TABLE `student` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `myEmail` varchar(45) DEFAULT NULL,
+    `myPassword` varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+)
